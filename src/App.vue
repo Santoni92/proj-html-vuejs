2388e779895abc02f837127006e5c656904e1a3f
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-26 15:26:37 +0200

    creato progetto vue cli e componente per gestire l'header; in App.vue posto array di stringhe cos' da passare,tramite props, le voci di menu al componente che gestisce l'header

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HeaderComponent :vociMenuHeader="vociMenuHeader"/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      vociMenuHeader:['Home','Pages','Courses','Features','Blog','Shop']
+    }
+  },
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="voceMenu in vociMenuHeader" :key="voceMenu">
+        {{ voceMenu }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    vociMenuHeader:Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container{
+  max-width:1170px;
+  margin:0 auto;
+  display:flex;
+  ul {
+    display: flex;
+    list-style-type: none;
+    li{
+      padding:5px;
+    }
+  }
+}
+</style>
